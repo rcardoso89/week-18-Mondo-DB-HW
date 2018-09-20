69d3e735c3ed5245914b40565e44effefc0fe31a
Xubiz <rc4514507@gmail.com>
2018-09-20 18:13:21 -0400

    fix for mongo DB

--- server.js
+++ server.js
@@ -12,10 +12,17 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static('public'));
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoosescraper";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 
 
 //Database configuration
-mongoose.connect('mongodb://localhost/mongoosescraper');
 var db = mongoose.connection;
 
 db.on('error', function (err) {
