3749626495f16ab0c16400a3c4fb9d7fb4886b9e
Xubiz <rc4514507@gmail.com>
2018-10-02 18:04:36 -0400

    fix for heroku

--- models/Article.js
+++ models/Article.js
@@ -1,20 +1,29 @@
-var mongoose = require('mongoose');
+// Require mongoose
+var mongoose = require("mongoose");
+// Create Schema class
 var Schema = mongoose.Schema;
 
+// Create article schema
 var ArticleSchema = new Schema({
+  // title is a required string
   title: {
-    type:String,
-    required:true
+    type: String,
+    required: true
   },
+  // link is a required string
   link: {
-    type:String,
-    required:true
+    type: String,
+    required: true
   },
+  // This only saves one note's ObjectId, ref refers to the Note model
   note: {
-      type: Schema.Types.ObjectId,
-      ref: 'Note'
+    type: Schema.Types.ObjectId,
+    ref: "Note"
   }
 });
 
-var Article = mongoose.model('Article', ArticleSchema);
+// Create the Article model with the ArticleSchema
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the model
 module.exports = Article;
--- public/app.js
+++ public/app.js
@@ -1,49 +1,72 @@
-$.getJSON('/articles', function(data) {
-  for (var i = 0; i<data.length; i++){
-    $('#articles').append('<p data-id="' + data[i]._id + '">'+ data[i].title + '<br />'+ data[i].link + '</p>');
+// Grab the articles as a json
+$.getJSON("/articles", function(data) {
+  // For each one
+  for (var i = 0; i < data.length; i++) {
+    // Display the apropos information on the page
+    $("#articles-list").append("<li class='eachArticle' data-id='" + data[i]._id + "'>" +"<strong>"+ data[i].title +"</strong>" + "<br />" + data[i].link + "</li>");
   }
 });
 
 
-$(document).on('click', 'p', function(){
-  $('#notes').empty();
-  var thisId = $(this).attr('data-id');
+// Whenever someone clicks a p tag
+$(document).on("click", "li", function() {
+  // Empty the notes from the note section
+  $("#notes").empty();
+  // Save the id from the p tag
+  var thisId = $(this).attr("data-id");
 
+  // Now make an ajax call for the Article
   $.ajax({
     method: "GET",
-    url: "/articles/" + thisId,
+    url: "/articles/" + thisId
   })
-    .done(function( data ) {
+    // With that done, add the note information to the page
+    .done(function(data) {
       console.log(data);
-      $('#notes').append('<h2>' + data.title + '</h2>');
-      $('#notes').append('<input id="titleinput" name="title" >');
-      $('#notes').append('<textarea id="bodyinput" name="body"></textarea>');
-      $('#notes').append('<button data-id="' + data._id + '" id="savenote">Save Note</button>');
+      // The title of the article
+      $("#notes").append("<h2>" + data.title + "</h2>");
+      // An input to enter a new title
+      $("#notes").append("<input id='titleinput' name='title' >");
+      // A textarea to add a new note body
+      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      // A button to submit a new note, with the id of the article saved to it
+      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
-      if(data.note){
-        $('#titleinput').val(data.note.title);
-        $('#bodyinput').val(data.note.body);
+      // If there's a note in the article
+      if (data.note) {
+        // Place the title of the note in the title input
+        $("#titleinput").val(data.note.title);
+        // Place the body of the note in the body textarea
+        $("#bodyinput").val(data.note.body);
       }
     });
 });
 
-$(document).on('click', '#savenote', function(){
-  var thisId = $(this).attr('data-id');
+// When you click the savenote button
+$(document).on("click", "#savenote", function() {
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
 
+  // Run a POST request to change the note, using what's entered in the inputs
   $.ajax({
     method: "POST",
     url: "/articles/" + thisId,
     data: {
-      title: $('#titleinput').val(),
-      body: $('#bodyinput').val()
+      // Value taken from title input
+      title: $("#titleinput").val(),
+      // Value taken from note textarea
+      body: $("#bodyinput").val()
     }
   })
-    .done(function( data ) {
+    // With that done
+    .done(function(data) {
+      // Log the response
       console.log(data);
-      $('#notes').empty();
+      // Empty the notes section
+      $("#notes").empty();
     });
 
-
-  $('#titleinput').val("");
-  $('#bodyinput').val("");
+  // Also, remove the values entered in the input and textarea for note entry
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
 });
